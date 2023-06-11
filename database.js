82b1a27e8c47357d2767c47fda373d6e420514b3
Anugrah K M <anugrahanu03@gmail.com>
2023-06-11 19:37:16 +0530

    completed index page

---
+++ database.js
@@ -0,0 +1,76 @@
+
+const itemTable =
+  [
+    [
+      "101",
+      {
+        id: "101",
+        name: "Camera Bag",
+        oldPrice: 200,
+        currPrice: 99,
+        img: "bag.png"
+      }
+    ],
+    [
+      "102",
+      {
+        id: "102",
+        name: "Football",
+        oldPrice: "",
+        currPrice: 49,
+        img: "ball.png"
+      }
+    ],
+    [
+      "103",
+      {
+        id: "103",
+        name:"fan",
+        oldPrice:70,
+        currPrice:49,
+        img:"fan.png"
+      }
+    ],
+    [
+      "104",
+      {
+        id: "104",
+        name:"camera",
+        oldPrice:"",
+        currPrice:299,
+        img:"camera.png"
+      }
+    ],
+    [
+      "105",
+      {
+        id: "105",
+        name: "jacket",
+        oldPrice: "99",
+        currPrice: 89,
+        img: "jacket.png"
+      }
+    ],
+    [
+      "106",
+      {
+        id: "106",
+        name:" shirt",
+        oldPrice: "",
+        currPrice: 49,
+        img: "shirt.jpg"
+      }
+    ],
+    [
+      "102",
+      {
+        id: "107",
+        name: "Football",
+        oldPrice: "",
+        currPrice: 49,
+        img: "ball.png"
+      }
+    ]
+  ]
+
+const dataItemMap = new Map(itemTable);
--- main.js
+++ main.js
@@ -1,24 +1,73 @@
-// HeaderScrollShadow();
+// import { dataItems } from "./database"
 
-const navbar = document.querySelector(".nav-list");
-const menuButton = document.querySelector(".menu");
+const dataItems = [...dataItemMap.values()]
+const savedItems = [];
 
-// console.log("clicked")
-menuButton.addEventListener('click', ()=>{
+HeaderScrollShadow();
+MenuToggle();
+FormSubmit();
+AddItemsToShop();
 
-  const visibility = navbar.getAttribute("data-visible");
-  if(visibility === "false"){
-    navbar.setAttribute("data-visible", true);
-    menuButton.children[0].src = "./assets/icons/close.png";
-  }else if(visibility === "true"){
-    navbar.dataset.visible = false;
-    menuButton.children[0].src = "./assets/icons/menu.png";
-  }
-
+const addToCartButton = document.querySelectorAll(".cart-btn");
+addToCartButton.forEach(button => {
+  button.addEventListener('click', AddItemToCart)
 })
 
+/////////////////////////////////////////////////////////////////
 
-function HeaderScrollShadow(){
+
+function AddItemToCart(e) {
+  AddedCartButtonStyles(e);
+  IncrementCartCount(e.target);
+  SaveAddedItems(e.target);
+}
+
+
+function SaveAddedItems(element) {
+  // console.log(element.parentElement) //Both Works
+
+  const itemId = element.closest("[data-item-id]").getAttribute("data-item-id");
+  savedItems.push({ id: itemId, qty: 1 });
+  console.log(savedItems);
+  localStorage.setItem("cart", JSON.stringify(savedItems));  ///Stringify for localStorage
+}
+
+
+function AddedCartButtonStyles(button) {
+  button.target.style.backgroundColor = "#FF5E0E"
+  button.target.style.paddingInline = "4em"
+  button.target.style.pointerEvents = "none"
+  button.target.innerText = "Added"
+}
+
+
+function IncrementCartCount(button) {
+  const cartButton = document.querySelector("[data-shopping-cart]");
+  let currVal = cartButton.getAttribute("data-cart-items").match(/\d+/)[0];
+  cartButton.setAttribute("data-cart-items", `${++currVal}`);
+  if (currVal > 99) {
+    cartButton.setAttribute("data-cart-items", "99+");
+  }
+  // console.log(currVal);
+}
+
+
+function FormSubmit() {
+  const formSubmit = document.querySelector(".form-submit");
+  formSubmit.addEventListener('click', () => {
+    const inputField = formSubmit.nextElementSibling;
+    if (inputField.value) {
+      inputField.value = "";
+      inputField.placeholder = " Thank You!"
+      inputField.readOnly = true;
+    } else {
+      inputField.placeholder = " Please enter your email!"
+    }
+  })
+}
+
+
+function HeaderScrollShadow() {
   window.addEventListener('scroll', () => {
     let scroll = this.scrollY;
     if (scroll > 10) {
@@ -27,4 +76,44 @@ function HeaderScrollShadow(){
       document.querySelector("header").classList.remove("shadow");
     }
   })
+}
+
+
+function MenuToggle() {
+  const navbar = document.querySelector(".nav-list");
+  const menuButton = document.querySelector(".menu");
+
+  // console.log("clicked")
+  menuButton.addEventListener('click', () => {
+
+    const visibility = navbar.getAttribute("data-visible");
+    if (visibility === "false") {
+      navbar.setAttribute("data-visible", true);
+      menuButton.children[0].src = "./assets/icons/close.png";
+    } else if (visibility === "true") {
+      navbar.dataset.visible = false;
+      menuButton.children[0].src = "./assets/icons/menu.png";
+    }
+  })
+}
+
+
+function AddItemsToShop() {
+  const values = GetItemsAsHtml();
+  // console.log(values);
+  const shoppingGrid = document.querySelector(".shopping-grid");
+  shoppingGrid.innerHTML = values;
+}
+function GetItemsAsHtml() {
+  return dataItems.map(item => {
+    let { id, name, oldPrice, currPrice, img } = item;
+    return (
+      `<div class="item" data-item-id = ${id}>
+          <div class="item-image"><img src="./assets/items/${img}" alt=""></div>
+          <h3 class="item-name">${name}</h3>
+          <p class="price"><span class="old-price">${oldPrice}</span>${currPrice}$</p>
+          <button class="cart-btn">Add to Cart</button>
+      </div>
+      `)
+  }).join("")
 }
