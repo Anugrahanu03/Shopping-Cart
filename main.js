a6ea44f54af63f1e45ea654970c283a7e13722a3
Anugrah K M <anugrahanu03@gmail.com>
2023-06-12 21:16:48 +0530

    Cart count on window load

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // import { dataItems } from "./database"
 
 const dataItems = [...dataItemMap.values()]
-const savedItems = [];
+const userCart = JSON.parse(localStorage.getItem("cart")) || [];
 
 HeaderScrollShadow();
 MenuToggle();
 FormSubmit();
 AddItemsToShop();
+ShowCartCount();
 
 const addToCartButton = document.querySelectorAll(".cart-btn");
 addToCartButton.forEach(button => {
   button.addEventListener('click', AddItemToCart)
 })
+
 
 /////////////////////////////////////////////////////////////////
 
@@ -25,11 +27,21 @@ function AddItemToCart(e) {
 
 function SaveAddedItems(element) {
   // console.log(element.parentElement) //Both Works
-
+  const itemsInCart = JSON.parse(localStorage.getItem("cart")) || [];
   const itemId = element.closest("[data-item-id]").getAttribute("data-item-id");
-  savedItems.push({ id: itemId, qty: 1 });
-  console.log(savedItems);
-  localStorage.setItem("cart", JSON.stringify(savedItems));  ///Stringify for localStorage
+  const itemIndex = itemsInCart.findIndex((item)=>{
+    return item.id === itemId;
+  });
+
+  if(itemIndex === -1) userCart.push({ id: itemId, qty: 1 });
+  localStorage.setItem("cart", JSON.stringify(userCart));  ///Stringify for localStorage
+
+  // if(itemIndex > -1) {
+  //   ++itemsInCart[itemIndex].qty;
+  //   console.log("added")
+  // }else {
+  //   userCart.push({ id: itemId, qty: 1 })
+  // }
 }
 
 
@@ -99,21 +111,48 @@ function MenuToggle() {
 
 
 function AddItemsToShop() {
+
   const values = GetItemsAsHtml();
-  // console.log(values);
   const shoppingGrid = document.querySelector(".shopping-grid");
   shoppingGrid.innerHTML = values;
+  // console.log(values);
 }
+
 function GetItemsAsHtml() {
   return dataItems.map(item => {
+
+    let buttonText = "Add to Cart";
+    let buttonNonClickable = "";
     let { id, name, oldPrice, currPrice, img } = item;
+
+    const alreadyInCart = userCart.some((x)=>{
+      return x.id === item.id;
+    });
+
+    // console.log(alreadyInCart);
+
+    if(alreadyInCart){
+      buttonText = "Already in Cart";
+      buttonNonClickable = "non-clickable";
+    }
     return (
       `<div class="item" data-item-id = ${id}>
           <div class="item-image"><img src="./assets/items/${img}" alt=""></div>
           <h3 class="item-name">${name}</h3>
           <p class="price"><span class="old-price">${oldPrice}</span>${currPrice}$</p>
-          <button class="cart-btn">Add to Cart</button>
+          <button class="cart-btn ${buttonNonClickable}">${buttonText}</button>
       </div>
       `)
   }).join("")
+}
+
+function ShowCartCount(){
+  // console.log(itemsInCart)
+  // const totalCount = userCart.reduce((acc, value) => {
+  //   return acc + value.qty;
+  // }, 0);
+
+  const noOfItemsInCart = userCart.length;
+  const cartButton = document.querySelector("[data-shopping-cart]");
+  cartButton.setAttribute("data-cart-items", noOfItemsInCart);
 }
